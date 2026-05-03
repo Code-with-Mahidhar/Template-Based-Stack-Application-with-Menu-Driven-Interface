af03c61ffc0bdd33e581cd77288879cb4dc4870a
Bandre Mahidhar Reddy <150232309+Code-with-Mahidhar@users.noreply.github.com>
2026-05-03 11:41:57 +0530

    Add output log for stack application in C++
    This file contains the output log of a stack application in C++. It demonstrates various operations such as push, pop, and display stack elements.

---
+++ myoutputs.cpp
@@ -0,0 +1,117 @@
+
+/*
+v25be3b5@system7:~/v25be3b5/cpp/miniprojects/miniproject02$ c++ program.cpp
+v25be3b5@system7:~/v25be3b5/cpp/miniprojects/miniproject02$ ls
+a.out  program.cpp
+v25be3b5@system7:~/v25be3b5/cpp/miniprojects/miniproject02$ ./a.out
+
+--- MAIN MENU ---
+1.int 2.char 3.float 4.double 5.string 6.exit
+Select option: 1
+
+int stack application ready to use
+1.push 2.pop 3.display_stack 4.main_menu
+Select option: 1
+Enter element: 40
+Element pushed: 40
+
+int stack application ready to use
+1.push 2.pop 3.display_stack 4.main_menu
+Select option: 1
+Enter element: 20
+Element pushed: 20
+
+int stack application ready to use
+1.push 2.pop 3.display_stack 4.main_menu
+Select option: 1
+Enter element: 30
+Element pushed: 30
+
+int stack application ready to use
+1.push 2.pop 3.display_stack 4.main_menu
+Select option: 3
+Stack elements (top to bottom):
+| 30 |
+| 20 |
+| 40 |
+
+int stack application ready to use
+1.push 2.pop 3.display_stack 4.main_menu
+Select option: 1
+Enter element: 50
+Element pushed: 50
+
+int stack application ready to use
+1.push 2.pop 3.display_stack 4.main_menu
+Select option: 1
+Enter element: 10
+Element pushed: 10
+
+int stack application ready to use
+1.push 2.pop 3.display_stack 4.main_menu
+Select option: 3
+Stack elements (top to bottom):
+| 10 |
+| 50 |
+| 30 |
+| 20 |
+| 40 |
+
+int stack application ready to use
+1.push 2.pop 3.display_stack 4.main_menu
+Select option: 1
+Enter element: 60
+Stack overflow
+
+int stack application ready to use
+1.push 2.pop 3.display_stack 4.main_menu
+Select option: 5
+please read the options carefully
+
+int stack application ready to use
+1.push 2.pop 3.display_stack 4.main_menu
+Select option: 3
+Stack elements (top to bottom):
+| 10 |
+| 50 |
+| 30 |
+| 20 |
+| 40 |
+
+int stack application ready to use
+1.push 2.pop 3.display_stack 4.main_menu
+Select option: 4
+
+--- MAIN MENU ---
+1.int 2.char 3.float 4.double 5.string 6.exit
+Select option: 9
+last chance please take care
+
+--- MAIN MENU ---
+1.int 2.char 3.float 4.double 5.string 6.exit
+Select option: 8
+thanks for using our application. please read the manual and come back properly.
+*/
+
+//error_handling
+
+/*
+v25be3b5@system7:~/v25be3b5/cpp/miniprojects/miniproject02$ ./a.out
+
+--- MAIN MENU ---
+1.int 2.char 3.float 4.double 5.string 6.exit
+Select option: 9
+please read the options carefully
+
+--- MAIN MENU ---
+1.int 2.char 3.float 4.double 5.string 6.exit
+Select option: 8
+last chance please take care
+
+--- MAIN MENU ---
+1.int 2.char 3.float 4.double 5.string 6.exit
+Select option: 7
+thanks for using our application. please read the manual and come back properly.
+*/
+~
+~
